91919877c9d6ee7cad84c34f3b3a525d54851b0d
Christian Pearson <lcpearso@umich.edu>
2021-12-26 12:04:22 -0500

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = false;
+    string favoriteString = "String";
+    address favoriteAddress = 0x6acF908B75713d38E7Fabc9DB309721CEF12A603;
+
+}
