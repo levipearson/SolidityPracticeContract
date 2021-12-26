aa80d2ca98796c9c381b722abe100d0b84808934
Christian Pearson <lcpearso@umich.edu>
2021-12-26 16:29:52 -0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,10 +1,13 @@
+//SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
 
-    uint256 favoriteNumber = 5;
-    bool favoriteBool = false;
-    string favoriteString = "String";
-    address favoriteAddress = 0x6acF908B75713d38E7Fabc9DB309721CEF12A603;
+//this will get initalized to 0!
+uint256 public favoriteNumber;
 
+function store(uint256 _favoriteNumber) public {
+    favoriteNumber = _favoriteNumber;
+    }
 }
